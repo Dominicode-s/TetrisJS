b4db70bb7d4c7da7da7ae2ec32167536dc929d86
Dominicode-s <60432849+Dominicode-s@users.noreply.github.com>
2020-05-26 14:48:54 +0100

    Added func
    Game now allows for moving left and right. Pausing the game and dropping the current tetris shape.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () =>{ //when page is loaded
     const grid = document.querySelector('.grid');
     const width = 10;
-    const ScoreDisplay = document.querySelector('#score');
-    const StartBtn = document.querySelector('#start-button')
+    const scoreDisplay = document.querySelector('#score');
+    const startBtn = document.querySelector('#start-button')
+    let nextRandom = 0
+    timerId = 0
 
     let squares = document.querySelectorAll('.grid div');
 
@@ -44,7 +46,7 @@ document.addEventListener('DOMContentLoaded', () =>{ //when page is loaded
         [1,width,width+1,width*2]
     ]
 
-    const tetrisShapes = [lTetris, zTetris, tTetris, oTetris, iTertis]
+    const tetrisShapes = [lTetris, zTetris, tTetris, oTetris, iTertis] //array of shapes
 
     let currentPosition = 4;
     let currentRotation = 0;
@@ -66,14 +68,22 @@ document.addEventListener('DOMContentLoaded', () =>{ //when page is loaded
         })
     }
 
-
-    //Start the game. interval 1 sec
-
-    StartBtn.onclick = function() {
-        StartBtn.innerHTML = 'PLAYING'
-        timerId = setInterval(moveDown, 1000)
+    //assign functions to keycodes
+    function control(e) {
+        if(e.keyCode === 37){
+            moveLeft()
+        }
+        if(e.keyCode === 39){
+            moveRight()
+        }
+        if(e.keyCode === 38){
+            rotateRight()
+        }
+        if(e.keyCode === 40){
+            moveDown()
+        }
     }
-
+    document.addEventListener("keyup", control)
 
     function moveDown() {
             undraw()
@@ -88,12 +98,95 @@ document.addEventListener('DOMContentLoaded', () =>{ //when page is loaded
         if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
             current.forEach(index => squares[currentPosition + index].classList.add('taken'))
             //start new tetris block falling
-            random = Math.floor(Math.random() * tetrisShapes.length)
+            random = nextRandom
+            nextRandom = Math.floor(Math.random() * tetrisShapes.length)
             current = tetrisShapes[random][currentRotation]
             currentPosition = 4
             draw()
+            displayTetromino()
+
         }
     }
 
+
+    //move tetris shape left
+
+    function moveLeft() {
+        undraw()
+        const isLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+        if(!isLeftEdge) currentPosition -=1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+            currentPosition += 1
+        }
+
+        draw()
+    }
+
+    function moveRight() {
+        undraw()
+        const isRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+
+        if(!isRightEdge) currentPosition +=1
+
+        if(current.some(index => squares[currentPosition+index].classList.contains('taken'))){
+            currentPosition -=1
+        }
+        draw()
+    }
+
+    function rotateRight() {
+        undraw()
+
+        currentRotation ++;
+        if(currentRotation === current.length){
+            currentRotation = 0;
+        }
+
+        current = tetrisShapes[random][currentRotation];
+
+        console.log(current)
+        draw()
+    }
+
+    //show next tetris shape
+
+    const displaySquares = document.querySelectorAll('.mini-grid div')
+    const displayWidth = 4
+    let displayIndex = 0
+
+    //the Tetris shapes without rotations
+
+    const upNextTetrisShapes = [
+        [1, displayWidth+1, displayWidth*2+1, 2], //L
+        [0,1, displayWidth+1,displayWidth+2], //Z
+        [1,displayWidth,displayWidth+1,displayWidth+2], //T
+        [0, 1, displayWidth, displayWidth+1], //O
+        [displayWidth,displayWidth+1,displayWidth+2,displayWidth+3], //I
+    ]
+
+    function displayTetromino() {
+        displaySquares.forEach(square => {
+            square.classList.remove('tetromino')
+        })
+
+        upNextTetrisShapes[nextRandom].forEach(index => {
+            displaySquares[displayIndex + index].classList.add('tetromino')
+        })
+
+    }
+
+    startBtn.addEventListener('click', () => {
+        if(timerId) {
+            clearInterval(timerId)
+            timerId = null
+        } else {
+            draw()
+            timerId = setInterval(moveDown, 1000)
+            nextRandom = Math.floor(Math.random()*tetrisShapes.length)
+            displayTetromino()
+        }
+    })
 
 });
